2288e8a9bb89441a0358f05b3e25a505123acda8
JDZ22503 <131354825+JDZ22503@users.noreply.github.com>
2024-10-25 18:42:33 +0530

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+  theme: {
+    extend: {
+      colors: {
+        'grass-green': '#7CFC00',
+      },
+    }
+  }
+}
---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+// App.jsx
+import React from 'react';
+import MainBody from './components/MainBody';
+import FAQ from './components/FAQ/FAQ';
+import Footer from './components/Footer/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        
+        <MainBody />
+        <FAQ />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: '/contact',
+    element: (
+      <>
+        <Contact />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: '/about',
+    element: (
+      <>
+     
+        <About />
+        <Footer />
+      </>
+    ),
+  },
+]);
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden">
+      <RouterProvider router={router} />
+    </main>
+  );
+};
+
+export default App;
